07211b51ded0804366fc4f9f7d73873c21b3139d
herbertz0 <howherbertzo@gmail.com>
2015-05-23 13:29:16 +0200

    Update for correcting codes

--- main_polynomials.c
+++ main_polynomials.c
@@ -1,20 +1,28 @@
 /*
  * Main file.
  */
-#define DATA_SIZE 32
-#define DATA
+
+//Import all modules and libs
+#include "../lib_data/data.h"
+#include "../lib_utilities/utilities.h"
+
+#include "src/hamming.h"
+#include "src/matrix.h"
+
+// Import configuration
+#include "hamming_config.h"
 #include "config.h"
 #include "src/onewire.h"
 #include "../lib_data/data.h"
 #include "../lib_uart/uart.h"
-#include <avr/.h>
+#include <avr/interrupt.h>
 
 uint32_t nb_data;
-struct Data* data;
+struct Data* _data;
 
-int main(void)interrupt
+int main(void)
 {
-    data = data_generate(DATA_SIZE);
+    _data = data_generate(32);
     nb_data = 0;
 
 	#ifdef DEBUG
@@ -38,8 +46,7 @@ int main(void)interrupt
 	    if(nb_data == 32)
         {
             nb_data = 0;
-            data_show(data);
-            //decode(data)
+            data_show(_data);
         }
         _delay_ms(3000);
 	}
@@ -51,17 +58,17 @@ ISR (INT1_vect) // Falling edge detected
     _delay_us(T/4);
     if(bus_read())
     {
-        if(nb_data < 32)
+        if(nb_data < 31)
         {
-            data_set(nb_data, 1, data);
+            data_set(nb_data, 1, _data);
         }
         nb_data += 1;
     }
     else
     {
-        if(nb_data < 32)
+        if(nb_data < 31)
         {
-            data_set(nb_data, 0, data);
+            data_set(nb_data, 0, _data);
         }
         nb_data += 1;
     }
